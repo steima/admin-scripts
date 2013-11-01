f059fb5ee1c7ac4570aa4ec56840a29352485244
steima <matthias.steinbauer@gmail.com>
2013-11-01 18:01:17 +0100

    First tests on the setup routine

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ admin-scripts
 =============
 
 A set of bash scripts that help admins to manage Xen based Debian servers
+
+WARNING! Under heavy development, the author does not recommend to use these scripts until this
+message gets removed!
+
+Setup:
+
+wget https://raw.github.com/steima/admin-scripts/master/bin/setup.sh -v -O setup.sh --no-check-certificate && chmod +x ./setup.sh && ./setup.sh && rm -rf setup.sh
--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,13 @@
 # This is the setup and update tool for machines that are using the admin scripts
 # One needs to specifiy the role of the current machine to get designated role scripts added to the setup
 
+configurationDir="/etc/admin-scripts/"
 
+echo -n "Checking if this is a new setup ..."
+
+if [ -d "${configurationDir}" ] ; then
+	echo "Upgrade"
+	echo "Upgrading existing installation"
+else
+	echo "New"
+fi
